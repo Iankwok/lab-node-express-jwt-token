9d374ce636d8ceeaed97f78ec0b21fa49998690b
“Ian <ianyhkwok@hotmail.com>
2015-11-18 17:06:33 +0800

    Completed

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ expressJWT  = require('express-jwt'),
 jwt         = require('jsonwebtoken'),
 bcrypt      = require('bcrypt'),
 app         = express(),
-logger      = require('morgan');
+logger      = require('morgan'),
+Agent       = require('./models/agent');
+
+var secret = "OnHerMajestysSecretService";
 
 mongoose.connect('mongodb://localhost:27017/MI6');
 
@@ -22,6 +25,39 @@ if (app.get('env') === 'development') {
     });
   });
 }
+
+app.use('/api/agents/:id', expressJWT({secret: secret}), function (error, request, response, next) {
+  // send an appropriate status code & JSON object saying there was an error, if there was one.
+  if (error.name === 'UnauthorizedError') {
+    response.status(401).json({message: 'You need an authorization token to view confidential information. (No Token)'});
+  }
+  next();
+});
+
+app.use('/api/safehouses', expressJWT({secret: secret}), function (error, request, response, next) {
+  // send an appropriate status code & JSON object saying there was an error, if there was one.
+  if (error.name === 'UnauthorizedError') {
+    response.status(401).json({message: 'You need an authorization token to view confidential information. (No Token)'});
+  }
+  next();
+});
+
+app.post('/api/authorizations', function(request, response){
+  var agentParams = request.body;
+  Agent.findOne({codename: agentParams.codename}, function(error, agent){
+    if(!agent) response.status(404).json({message: "There is no agent by that codename."});
+
+    agent.authenticate(agentParams.name, function(error, isMatch){
+      if (isMatch){
+        var token = jwt.sign(agent, secret);
+        return response.json({agent: agent, token: token});
+      } else {
+        return response.status(401).json({message: "You don't have clearence, agent."});
+      }
+    });
+  });
+});
+
 
 var routes = require('./config/routes');
 app.use('/api', routes);
--- controllers/safehousesController.js
+++ controllers/safehousesController.js
@@ -2,7 +2,8 @@ var Safehouse = require('../models/safehouse');
 
 // INDEX
 function getAll(request,response){
-  Safehouse.find(function(error, safehouses){
+  var id = request.user.id;
+  Safehouse.find({agents: id},function(error, safehouses){
     if (error) response.json({message: 'There is no MI6, and there are no safehouses here.'});
 
     response.json({safehouses: safehouses});
@@ -21,9 +22,9 @@ function createSafehouse(request,response){
 
 // SHOW
 function getSafehouse(request, response){
-  var id = request.params.id;
-
-  Safehouse.findOne({_id: id}, function(error, safehouse){
+  // var id = request.params.id;
+ var id = request.user.id;
+  Safehouse.findOne({agents: id}, function(error, safehouse){
 
     if(error) { response.status(404).json({message: 'You seem to be mistaken, we have no safehouse with that ID.'}); }
     else if (!safehouse) { response.status(404).json({message: 'You seem to be mistaken, we have no safehouse with that ID.'}); }
